80a98d72b374d8a1309e30a2417cf655f928ffd2
DaKnig <37626476+DaKnig@users.noreply.github.com>
2021-06-18 09:25:30 +0300

    fixed indentation

--- contrib/bash_compl/_rgbasm.bash
+++ contrib/bash_compl/_rgbasm.bash
@@ -18,46 +18,46 @@ _rgbasm_completions() {
 	if [ "${current_word:0:1}-" == "--" ]
 	   # a flag- autocomplete to any flag
 	then
-	current_word=${current_word:1:10} # no flag is 10 chars long anyways
-	local flags=()
-	# generate a list of all flags without the leading minus
-	# # compgen doesnt like getting minuses in the last argument - so we remove the minus and add it back later
-	single_letter_flags="EhLVvwbDgiMoprW"
-	for i in $(seq 1 ${#single_letter_flags})
-	do
-		flags+=("${single_letter_flags:i-1:1}")
-	done
-	flags+=("MG" "MP" "MT" "MQ")
+		current_word=${current_word:1:10} # no flag is 10 chars long anyways
+		local flags=()
+		# generate a list of all flags without the leading minus
+		# # compgen doesnt like getting minuses in the last argument - so we remove the minus and add it back later
+		single_letter_flags="EhLVvwbDgiMoprW"
+		for i in $(seq 1 ${#single_letter_flags})
+		do
+			flags+=("${single_letter_flags:i-1:1}")
+		done
+		flags+=("MG" "MP" "MT" "MQ")
 
-	local f="${flags[@]}"
+		local f="${flags[@]}"
 
-	local completions=$( compgen -W "$f" "$current_word" )
-	# add the - back to the flags...
-	COMPREPLY+=( $(echo $completions | sed -E "s/(^| )/ -/g") )
+		local completions=$( compgen -W "$f" "$current_word" )
+		# add the - back to the flags...
+		COMPREPLY+=( $(echo $completions | sed -E "s/(^| )/ -/g") )
 	elif [ "$last_word" == "-i" ]
-	 # set include path - suggest folders
+		 # set include path - suggest folders
 	then
-	COMPREPLY=( $( compgen -A directory $current_word ) ) # use builtin completion for directories in compgen
+		COMPREPLY=( $( compgen -A directory $current_word ) ) # use builtin completion for directories in compgen
 	elif [ "$last_word" == "-MQ" ] || [ "$last_word" == "-MT" ]
-	 # autocomplete to all files in that case
+		 # autocomplete to all files in that case
 	then
-	COMPREPLY=( $( compgen -A file $current_word ) )
+		COMPREPLY=( $( compgen -A file $current_word ) )
 	elif [ "$last_word" == "-W" ]
-	 # autocomplete to a warning
+		 # autocomplete to a warning
 	then
-	warnings=( "error" "all" "extra" "everything" "assert" "backwards-for" "builtin-args" "charmap-redef" )
-	warnings+=( "div" "empty-macro-arg" "empty-strrpl" "large-constant" "long-string" "macro-shift" )
-	warnings+=( "obsolete" "shift" "shift-amount" "truncation" "user" )
+		warnings=( "error" "all" "extra" "everything" "assert" "backwards-for" "builtin-args" "charmap-redef" )
+		warnings+=( "div" "empty-macro-arg" "empty-strrpl" "large-constant" "long-string" "macro-shift" )
+		warnings+=( "obsolete" "shift" "shift-amount" "truncation" "user" )
 
-	local f="${warnings[@]}"
-	COMPREPLY+=( $( compgen -W "$f" $current_word ) )
+		local f="${warnings[@]}"
+		COMPREPLY+=( $( compgen -W "$f" $current_word ) )
 	elif [ $words_len==$(($COMP_CWORD+1)) ]
-	 #autocomplete at the end of line (empty end) - only show files
+		 #autocomplete at the end of line (empty end) - only show files
 	then
-	COMPREPLY+=( $( compgen -A directory $current_word ) )
+		COMPREPLY+=( $( compgen -A directory $current_word ) )
 
-	local f="${asm_files[@]}"
-	COMPREPLY+=( $( compgen -A file $current_word | grep -E "\.(sm83|z80|asm|inc)$") )
+		local f="${asm_files[@]}"
+		COMPREPLY+=( $( compgen -A file $current_word | grep -E "\.(sm83|z80|asm|inc)$") )
 	fi
 }
 
